23266afe57701cf42773111a1bfdfb380c7087e4
Miguel Angel Civico <mcellinika@gmail.com>
2019-11-28 21:02:54 +0100

    adding invaders to the game engine

--- GameEngine.js
+++ GameEngine.js
@@ -6,32 +6,46 @@ class GameEngine {
     constructor() {
         this.heroShip = new HeroShip();
         this.arrayBullets = new Array();
-        document.onkeydown= (e) => this.keyboardInput(e); 
+        document.onkeydown = (e) => this.keyboardInput(e);
         this.bulletX = (this.heroShip.centerOfHeroShip - 1);
         this.bulletStartY = (this.heroShip.positionY + 2);
         this.bulletEndY = (this.heroShip.positionY + 8);
     }
-    
+
     keyboardInput(e) {
         var key = e.key;
-        if(key == "a" || key == "A") {
+        if (key == "a" || key == "A") {
             this.bulletX = (this.heroShip.centerOfHeroShip - 1);
             this.bulletStartY = (this.heroShip.positionY + 2);
             this.bulletEndY = (this.heroShip.positionY + 8);
             this.heroShip.moveLeft();
             this.heroShip.draw();
-        } else if(key == "d" || key == "D") {
+        } else if (key == "d" || key == "D") {
             this.bulletX = (this.heroShip.centerOfHeroShip - 1);
             this.bulletStartY = (this.heroShip.positionY + 2);
             this.bulletEndY = (this.heroShip.positionY + 8);
             this.heroShip.moveRight();
             this.heroShip.draw();
-        } else if(key == " ") {
+        } else if (key == " ") {
             var bullet = new Bullet(this.bulletX, this.bulletStartY, this.bulletX, this.bulletEndY);
             this.arrayBullets.push(bullet);
         }
     }
-    
+
+    addInvaders() {
+        var arrayInvaders = new Array();
+        var widthTotal = 0;
+        var heightTotal = 10
+        while (heightTotal < 200) {
+            arrayInvaders.push(new Invader(widthTotal, heightTotal));
+            widthTotal += 100;
+            if (widthTotal > 1000) {
+                widthTotal = 0;
+                heightTotal += 40
+            }
+        }
+    }
+
 }
 
 
@@ -51,16 +65,15 @@ window.onload = () => {
     gameBorder.id = "gameBorder";
     gameBorder.style = "fill:black;stroke-width:5;stroke:rgb(0,100,0)"
     document.getElementById('gameScreen').appendChild(gameBorder);
-
     var gameEngine = new GameEngine();
-    
+    gameEngine.addInvaders();
     setInterval(() => {
-        for(var bala of gameEngine.arrayBullets) {
+        for (var bala of gameEngine.arrayBullets) {
             bala.moveUp();
             bala.draw();
         }
     }, 100);
-    
+
 
 }
 
--- Invader.js
+++ Invader.js
@@ -1,7 +1,7 @@
 import { Bullet } from './Bullet.js';
 
 export class Invader {
-    constructor(positionX, positionY, height, width) {
+    constructor(positionX, positionY, height=30, width=60) {
         this.positionX = positionX;
         this.positionY = positionY;
         this.height = height;
@@ -9,7 +9,7 @@ export class Invader {
         this.tag = document.createElementNS("http://www.w3.org/2000/svg", "rect");
         this.tag.setAttribute("x", positionX);
         this.tag.setAttribute("y", positionY);
-        this.tag.setAttribute("fill", "green");
+        this.tag.setAttribute("fill", "yellow");
         this.tag.setAttribute("width", width);
         this.tag.setAttribute("height", height);
         document.getElementsByTagName("svg")[0].appendChild(this.tag);
