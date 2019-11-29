4569bca171e6b523f009d0c546322a7bb1d33a73
04emiir <emilio.nero3@gmail.com>
2019-11-29 13:39:34 +0100

    Major update for Space Invaders.
    Added tons of comments in the scripts. Edited some """confusing""" variable names. Invader are able to move (only in the X Axis). ***TO FIX*** After some time, the invaders get misplaced.

--- GameEngine.js
+++ GameEngine.js
@@ -6,6 +6,7 @@ class GameEngine {
     constructor() {
         this.heroShip = new HeroShip();
         this.arrayBullets = new Array();
+        this.arrayInvader = new Array();
         document.onkeydown= (e) => this.keyboardInput(e); 
     }
 
@@ -23,21 +24,21 @@ class GameEngine {
             var bulletX = this.heroShip.centerOfHeroShip;
             var bulletStartY = (this.heroShip.positionY - 5);
             var bulletEndY = (this.heroShip.positionY - 20);
-            var bullet = new Bullet(bulletX, bulletStartY, bulletX, bulletEndY);
+            var bullet = new Bullet(bulletX, bulletStartY, bulletEndY);
             this.arrayBullets.push(bullet);
         }
     }
 
     addInvaders() {
-        var arrayInvaders = new Array();
-        var widthTotal = 0;
-        var heightTotal = 10
+        var widthTotal = 300;
+        var heightTotal = 10;
         while (heightTotal < 200) {
-            arrayInvaders.push(new Invader(widthTotal, heightTotal));
+            var alienigena = new Invader(widthTotal, heightTotal)
+            this.arrayInvader.push(alienigena);
             widthTotal += 100;
-            if (widthTotal > 1000) {
-                widthTotal = 0;
-                heightTotal += 40
+            if (widthTotal > 700) {
+                widthTotal = 300;
+                heightTotal += 40;
             }
         }
     }
@@ -47,12 +48,14 @@ class GameEngine {
 
 
 window.onload = () => {
+    //  Creation of the gameScreen<svg>, set attributes and addition to the HTML.
     var gameScreen = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     gameScreen.setAttribute("width", "1000px");
     gameScreen.setAttribute("height", "700px");
     gameScreen.id = "gameScreen";
     document.body.appendChild(gameScreen);
 
+    //  Creation of the gameBorder<svg>, set attributes and addition to the gameBorder<svg>.
     var gameBorder = document.createElementNS("http://www.w3.org/2000/svg", "rect");
     gameBorder.setAttribute("x", "0");
     gameBorder.setAttribute("y", "0");
@@ -62,12 +65,14 @@ window.onload = () => {
     gameBorder.style = "fill:black;stroke-width:5;stroke:rgb(0,100,0)"
     document.getElementById('gameScreen').appendChild(gameBorder);
     
+    //  Creation of the sound effect for HeroShip bullet.
     var bulletSound = document.createElement("AUDIO");
     bulletSound.src = "bullet.mp3"
     bulletSound.controls = false;
     bulletSound.id = "bullet";
     document.body.appendChild(bulletSound);
     
+    //  Creation of the background music.
     var bgSound = document.createElement("AUDIO");
     bgSound.src = "howard.mp3"
     bgSound.controls = false;
@@ -85,6 +90,12 @@ window.onload = () => {
                 gameEngine.arrayBullets.splice(0, 1);
                 bulletShot.disappear();
             }
+        }
+        for(var alien of gameEngine.arrayInvader) {
+            alien.wholeMovement();
+        }
+        for(var alien of gameEngine.arrayInvader) {
+            alien.draw();
         }
     }, 10);
     
